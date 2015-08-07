ae7f638f48fdcde1df8e276e1463a9509e5ac17c
udaysagar2177 <udaysagar.2177@gmail.com>
2015-08-06 17:35:47 -0700

    Increased puppet quality score to 4.8

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,48 +4,47 @@
 #
 class install_collectd {
     
-    Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+    Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
     
     include 'install_collectd::install_repo'
 
     case $::osfamily {
         'Debian': {
 
-		package { 'collectd-core':
-			ensure => latest,
-			require => Class['install_repo']
-		}
-
-                class { '::collectd':
-                        purge        => true,
-                        recurse      => true,
-                        purge_config => true,
-                        version => latest,
-			require => Package['collectd-core']
+                package { 'collectd-core':
+                        ensure           => latest,
+                        require          => Class['install_repo']
                 }
-		
-        }
-	
-	'Redhat': {
 
                 class { '::collectd':
-                        purge        => true,
-                        recurse      => true,
-                        purge_config => true,
-                        version => latest,
-                	require => Class['install_repo']
-		}
-
-		package { ['collectd-disk', 'collectd-write_http']:
-			provider => 'yum',
-                        ensure => latest,
-			require => Class['install_repo']
-		}
+                        purge            => true,
+                        recurse          => true,
+                        purge_config     => true,
+                        version          => latest,
+                        require          => Package['collectd-core']
+                }
+    
         }
-	
+  
+        'Redhat': {
+
+                class { '::collectd':
+                        purge            => true,
+                        recurse          => true,
+                        purge_config     => true,
+                        version          => latest,
+                        require          => Class['install_repo']
+                }
+
+                package { ['collectd-disk', 'collectd-write_http']:
+                        ensure           => latest,
+                        provider         => 'yum',
+                        require          => Class['install_repo']
+                }
+        }
+  
         default: {
             fail("${::osfamily} is not supported.")
         }
     }
 }
-
--- manifests/install_repo.pp
+++ manifests/install_repo.pp
@@ -1,37 +1,44 @@
+# installs the SignalFx repositories on your system
+#
 class install_collectd::install_repo inherits install_collectd::repo_params {
 
-    Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+    Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
     case $::osfamily {
         'Debian': {
-                exec { 'add SignalFx ppa to software sources':
-                        # software-properties-common is the source package for add-apt-repository command (after Ubuntu 13.10)
-                        # python-software-properties is the source package for add-apt-repository command (before Ubuntu 13.10)
-                        command => 'apt-get update &&
-                                    apt-get -y install software-properties-common && 
-                                    apt-get -y install python-software-properties && 
-                                    add-apt-repository ppa:signalfx/collectd-release &&
-                                    apt-get update',
-                }
-        }
+            exec { 'add SignalFx ppa to software sources':
+                # software-properties-common is the source package for
+                # add-apt-repository command (after Ubuntu 13.10)
+                # python-software-properties is the source package for
+                # add-apt-repository command (before Ubuntu 13.10)
+                command     => 'apt-get update &&
+                               apt-get -y install software-properties-common && 
+                               apt-get -y install python-software-properties && 
+                               add-apt-repository ppa:signalfx/collectd-release &&
+                               apt-get update',
+                  }
+          }
 
         'Redhat':{
 
-            if $::operatingsystemmajrelease == "5" {
-                    exec { "install SignalFx repo  on centos 5":
-                            command => "yum -y install wget &&
-                                        wget ${repo_source} &&
-                                        yum -y install --nogpgcheck ${repo_name} &&
-                                        rm -f ${repo_name}"
+            if $::operatingsystemmajrelease == '5' {
+                    exec { 'install SignalFx repo  on centos 5':
+                    command     => "yum -y install wget &&
+                                   wget ${::repo_source} &&
+                                   yum -y install --nogpgcheck ${::repo_name} &&
+                                   rm -f ${::repo_name}"
                     }
             }
             else {
-                    package { $repo_name:
-                            provider => 'rpm',
-                            ensure => latest,
-                            source => $repo_source
+                    package { $::repo_name:
+                            ensure      => latest,
+                            provider    => 'rpm',
+                            source      => $::repo_source
                     }
             }
+        }
+        default: {
+              fail("${::osfamily} is not supported.")
         }
     }
 
